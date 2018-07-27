68b4277371c650e60323325ccf08a34b1da40c48
elysonpark <ypark04@risd.edu>
2018-07-27 12:11:04 +0900

    .btn-menu-act 클래스 토글 기능 추가

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,12 @@
-$(document).keyup(function(e){
-  if(e.keyCode === 27){
-    $('.grid').toggleClass('grid-show');
-  }
 
-})
+// $(document).keyup(function (e) {
+//   if (e.keyCode === 27) {
+//     $('.grid').toggleClass('grid-show');
+//   }
+// });
+
+var btn = $('.btn-menu');
+
+btn.click(function () {
+  $(this).toggleClass('btn-menu-act');
+});
